d7a803cabcfc90c070edc636e439d130128d1e5a
Luka Born <luka.born.2003@gmail.com>
2024-01-16 21:05:07 +0100

    added score ui element

--- src/ui/score.rs
+++ src/ui/score.rs
@@ -1,8 +1,9 @@
 use::bevy::prelude::*;
 
-use crate::utils::sets::Set;
+use crate::{utils::sets::Set, AppState};
 
-
+#[derive(Resource)]
+pub struct Score(pub u32);
 
 
 #[derive(Event)]
@@ -20,6 +21,7 @@ fn score_events(
     mut score_events: EventReader<ScoreEvent>,
     mut commands: Commands,
     camera_query: Query<(&GlobalTransform, &Camera)>, 
+    mut score: ResMut<Score>,
 ) {
     let Ok((transform, camera)) = camera_query.get_single() else { return; };
     for event in score_events.read() {
@@ -30,6 +32,8 @@ fn score_events(
             warn!("Could not get viewport position for node");
             continue;
         };
+
+        score.0 += event.score;
 
         commands.spawn((
             ScoreElement {
@@ -64,7 +68,35 @@ fn score_events(
     }
 }
 
+fn score_update(
+    mut score_query: Query<(&mut Style, &ScoreElement)>, 
+    time: Res<Time>,
+    window: Query<&Window>, 
+) {
+    const UI_ELEMENT_SPEED: f32 = 100.0;
 
+    let Ok(window) = window.get_single() else { return; };
+
+    for (mut style, _score_element) in &mut score_query {
+        let Val::Px(left) = style.left else { continue; };
+        let Val::Px(top) = style.top else { continue; };
+
+        let delta = Vec2 { 
+            x: window.width() / 2.0 - left, 
+            y: -top, 
+        }.normalize() * UI_ELEMENT_SPEED;
+
+        style.top = Val::Px(top + delta.y * time.delta_seconds());
+        style.left = Val::Px(left + delta.x * time.delta_seconds());
+    }
+}
+
+fn score_setup(
+    mut commands: Commands
+) {
+    commands.insert_resource(Score(0));
+
+}
 
 pub struct ScorePlugin;
 
@@ -74,6 +106,10 @@ impl Plugin for ScorePlugin {
             .add_event::<ScoreEvent>()
             .add_systems(Update, (
                 score_events.in_set(Set::ScoreEvents), 
+                score_update, 
+            ))
+            .add_systems(OnEnter(AppState::MainScene), (
+                score_setup, 
             ));
     }
 }
