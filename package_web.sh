d73dd50c831005dfe9deacc2bd2a99273b8d2b6b
Luka Born <luka.born.2003@gmail.com>
2024-04-25 21:40:16 +0200

    updated build config

--- package_web.sh
+++ package_web.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-cargo build --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/debug/test_game.wasm
+
+if [[ $1 == "release" ]] ; then
+    echo "Building in release mode..."
+    MODE="release"
+    cargo build --target wasm32-unknown-unknown --release
+elif [[ $1 == "debug" ]] ; then
+    echo "Building in debug mode..."
+    MODE="debug"
+    cargo build --target wasm32-unknown-unknown
+else 
+    echo "Usage: $0 <release|debug>"
+    exit 1
+fi
+
+echo "Generating wasm bindings..."
+wasm-bindgen --no-typescript --out-dir ./out/ --out-name "space_game" --target web ./target/wasm32-unknown-unknown/$MODE/test_game.wasm
+
+echo "Optimizing wasm..."
+wasm-opt -O ./out/space_game_bg.wasm -o ./out/space_game_bg.wasm
+
+echo "Copying assets..."
+rm ./out/assets -r
 cp ./assets ./out/ -r
--- run_web.sh
+++ run_web.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cargo build --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/debug/test_game.wasm
-cp ./assets ./out/ -r
+./package_web.sh debug
 python3.12 -m http.server 8080 --directory ./out/
